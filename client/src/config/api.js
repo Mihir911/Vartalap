f4ab9238d468a95182541e2e7b473f64a9f5d7be
Mihir911 <spygamer737@gmail.com>
2026-04-04 14:04:21 +0530

    fixed

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const API = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: `${import.meta.env.API_URL}`
 });
 
 API.interceptors.request.use((config) => {
